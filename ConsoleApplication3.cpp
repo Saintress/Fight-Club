cad067e6ea0f36dbe32624f2a61ca0c633d445ff
Saintress <poweska.bartosz@gmail.com>
2017-06-04 13:59:15 +0200

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,149 @@
+// ConsoleApplication3.cpp: Okreœla punkt wejœcia dla aplikacji konsoli.
+//
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+
+class BodyPart {
+	
+	int health;
+	int block;
+	int damage;
+	bool Active;
+
+public:
+	
+	BodyPart() { health = rand()%5 + 1; block = rand() % 5 + 1; damage = rand() % 5 + 1 ; Active = true; }
+	
+	void checkStatus() { if (health <= 0) Active = false; }
+	
+	int getHealth() { return health; }
+	
+	int getDamage() { return damage; }
+	
+	int getBlock() { return block; }
+	
+	bool getStatus() { return Active; }
+
+	int receiveDamage(int dmg) { return health-=dmg; }
+
+};
+
+
+class Fighter : public BodyPart {
+	
+	int bleedLimit;
+	int bleedCounter;
+	bool Alive;
+	BodyPart Head;
+	BodyPart leftArm;
+	BodyPart rightArm;
+	BodyPart leftLeg;
+	BodyPart rightLeg;
+
+public:
+	
+	BodyPart Body[5] = { Head, leftArm, rightArm, leftLeg, rightLeg };
+
+	Fighter() { Alive = true; bleedCounter = 0; bleedLimit = rand() % 5 + 6; }
+	
+	void IfAlive() {
+	    
+		Body[0].checkStatus();
+	    
+		if (Body[0].getStatus() == false)  Alive = false; //zawodnik ginie gdy staci glowe  
+	    
+		else if (bleedCounter >= bleedLimit) Alive = false; //lub straci za duzo krwi
+	    
+		else Alive = true;
+	};
+	
+	int Bleeding() {
+		
+		for (int i = 1; i < 5; i++)   //pomijamy glowe bo nie ma znaczenia w przypadku krwawienia (jesli stracilismy glowe to i tak nie zyjemy)
+		{
+			Body[i].checkStatus();
+			
+			if (Body[i].getStatus() == false) bleedCounter++;
+		}
+		
+		return bleedCounter;
+	}
+
+	bool getStatusFighter() { return Alive; }
+	
+};
+
+
+class Battle : public Fighter {
+	
+	int target;
+	int attackSource;
+
+public:
+	
+	void Attack(Fighter Warrior1, Fighter Warrior2) {
+
+		Warrior1.Bleeding(); // na poczatku tury zawodnik otrzymuje obrazenia od krwawienia
+
+		if (Warrior1.getStatusFighter() && Warrior2.getStatusFighter()) //jesli obaj zyja
+		{
+				attackSource = rand() % 5; 
+				
+				while (Warrior1.Body[attackSource].getStatus() == false) //jesli czesc ciala jest niektywna losujemy tak dlugo az znajdziemy aktywna
+				{
+					attackSource = rand() % 5;
+					if (Warrior1.Body[attackSource].getStatus() == true) break;
+				}
+				
+				target = rand() % 5;  
+
+				while (Warrior2.Body[target].getStatus() == false) //jesli czesc ciala jest niektywna losujemy tak dlugo az znajdziemy aktywna
+				{
+					target = rand() % 5;
+					if (Warrior2.Body[target].getStatus() == true) break;
+				}
+				
+				if (Warrior2.Body[target].getBlock() > rand() % 5 + 1) Warrior1.Body[attackSource].receiveDamage(Warrior1.Body[attackSource].getDamage()); //jesli blok sie powiedzie zostaje wyprowadzona kontra
+				
+				else if (Warrior2.Body[target].getBlock() == rand() % 5 + 1) Warrior2.Body[target].receiveDamage(ceil(Warrior1.Body[attackSource].getDamage() / 2)); //jesli blok powiedzie sie w polowie zawodnik blokujacy otrzyma polowe obrazen
+				
+				else Warrior2.Body[target].receiveDamage(Warrior1.Body[attackSource].getDamage()); //jesli blok sie nie powiedzie zawodnik otrzymuje pelne obrazenia
+				
+				Warrior1.Body[attackSource].checkStatus();
+				Warrior2.Body[target].checkStatus();
+			
+		}
+
+	}
+
+};
+
+class Tournament : public Battle {
+	
+	Fighter Duelists[64][64];
+
+public:
+
+	Tournament() {
+		for (int i = 0; i < 64; i++)
+		{
+         
+	    }
+	}
+
+};
+
+
+
+int main()
+{
+	srand(time(NULL));
+    return 0;
+}
+
