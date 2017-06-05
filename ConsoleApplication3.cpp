ae5d1eccd0f2261528fc656b974de9caf0583ca5
Saintress <poweska.bartosz@gmail.com>
2017-06-05 21:52:29 +0200

    Add files via upload

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -5,181 +5,228 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
+#define N 64
 
 using namespace std;
 
-
 class BodyPart {
-	
+private:
 	int health;
 	int block;
 	int damage;
 	bool Active;
 
 public:
-	
-	BodyPart() { health = rand()%5 + 1; block = rand() % 5 + 1; damage = rand() % 5 + 1 ; Active = true; }
-	
-	void checkStatus() { if (health <= 0) Active = false; }
-	
-	int getHealth() { return health; }
-	
-	int getDamage() { return damage; }
-	
-	int getBlock() { return block; }
-	
-	bool getStatus() { return Active; }
+	BodyPart()
+	{
+		health = rand() % 5 + 10000;
+		block = rand() % 5 + 1;
+		damage = rand() % 5 + 1;
+		Active = true;
+	}
 
-	int receiveDamage(int dmg) { return health-=dmg; }
+	int getHealth()
+	{
+		return health;
+	}
+
+	int getDamage()
+	{
+		return damage;
+	}
+
+	int getBlock()
+	{
+		return block;
+	}
+
+	bool getStatus()
+	{
+		return Active;
+	}
+
+	void receiveDamage(int dmg)
+	{
+		health -= dmg;
+		if (health <= 0)
+			Active = false;
+	}
 
 };
 
 
 class Fighter : public BodyPart {
-	
+private:
 	int bleedLimit;
 	int bleedCounter;
 	bool Alive;
-	BodyPart Head;
-	BodyPart leftArm;
-	BodyPart rightArm;
-	BodyPart leftLeg;
-	BodyPart rightLeg;
 
 public:
-	
-	BodyPart Body[5] = { Head, leftArm, rightArm, leftLeg, rightLeg };
+	BodyPart Body[5];
 
-	Fighter() { Alive = true; bleedCounter = 0; bleedLimit = rand() % 5 + 6; }
-	
-	void checkStatusFighter() {
-	    
-		Body[0].checkStatus();
-	    
-		if (Body[0].getStatus() == false)  Alive = false; //zawodnik ginie gdy staci glowe  
-	    
-		else if (bleedCounter >= bleedLimit) Alive = false; //lub straci za duzo krwi
-	    
-		else Alive = true;
-	};
-	
+	Fighter()
+	{
+		Alive = true;
+		bleedCounter = 0;
+		bleedLimit = rand() % 5 + 6;
+	}
+
+	void checkStatusFighter()
+	{
+		if (!Body[0].getStatus())
+			Alive = false; //zawodnik ginie gdy staci glowe
+		else if (bleedCounter >= bleedLimit)
+			Alive = false; //lub straci za duzo krwi
+		else
+			Alive = true;
+	}
+
 	int Bleeding() {
-		
+
 		for (int i = 1; i < 5; i++)   //pomijamy glowe bo nie ma znaczenia w przypadku krwawienia (jesli stracilismy glowe to i tak nie zyjemy)
 		{
-			Body[i].checkStatus();
-			
-			if (Body[i].getStatus() == false) bleedCounter++;
+			if (!Body[i].getStatus()) bleedCounter++; //krwawienie wzmaga sie wraz z utrata kolejnych czesci ciala
 		}
-		
 		return bleedCounter;
 	}
 
-	bool getStatusFighter() { return Alive; }
-	
-};
-
-
-class Battle : public Fighter {
-	
-	int target;
-	int attackSource;
-
-public:
-	
-	void Attack(Fighter Warrior1, Fighter Warrior2) {
-
-				attackSource = rand() % 5; 
-				
-				while (Warrior1.Body[attackSource].getStatus() == false) //jesli czesc ciala jest niektywna losujemy tak dlugo az znajdziemy aktywna
-				{
-					attackSource = rand() % 5;
-					if (Warrior1.Body[attackSource].getStatus() == true) break;
-				}
-				
-				target = rand() % 5;  
-
-				while (Warrior2.Body[target].getStatus() == false) //jesli czesc ciala jest niektywna losujemy tak dlugo az znajdziemy aktywna
-				{
-					target = rand() % 5;
-					if (Warrior2.Body[target].getStatus() == true) break;
-				}
-				
-				if (Warrior2.Body[target].getBlock() > rand() % 5 + 1) Warrior1.Body[attackSource].receiveDamage(Warrior1.Body[attackSource].getDamage()); //jesli blok sie powiedzie zostaje wyprowadzona kontra
-				
-				else if (Warrior2.Body[target].getBlock() == rand() % 5 + 1) Warrior2.Body[target].receiveDamage(ceil(Warrior1.Body[attackSource].getDamage() / 2)); //jesli blok powiedzie sie w polowie zawodnik blokujacy otrzyma polowe obrazen
-				
-				else Warrior2.Body[target].receiveDamage(Warrior1.Body[attackSource].getDamage()); //jesli blok sie nie powiedzie zawodnik otrzymuje pelne obrazenia
-				
-				Warrior1.Body[attackSource].checkStatus();
-				Warrior2.Body[target].checkStatus();
-
+	bool getStatusFighter()
+	{
+		return Alive;
 	}
 
-	void Match(Fighter Warrior1, Fighter Warrior2) {
-		
-		while (Warrior1.getStatusFighter() && Warrior2.getStatusFighter())
+};
+
+class Battle : public Fighter {
+public:
+	static void Attack(Fighter* Warrior1, Fighter* Warrior2)
+	{
+		int attackSource = rand() % 5;
+
+		while (!Warrior1->Body[attackSource].getStatus()) //jesli czesc ciala jest niektywna losujemy tak dlugo az znajdziemy aktywna
+			attackSource = rand() % 5;
+
+		int target = rand() % 5;
+
+		while (!Warrior2->Body[target].getStatus()) //jesli czesc ciala jest niektywna losujemy tak dlugo az znajdziemy aktywna
+			target = rand() % 5;
+
+		if (Warrior2->Body[target].getBlock() > rand() % 5 + 1) //jesli blok sie powiedzie zostaje wyprowadzona kontra
+			Warrior1->Body[attackSource].receiveDamage(Warrior1->Body[attackSource].getDamage());
+		else if (Warrior2->Body[target].getBlock() == rand() % 5 + 1) //jesli blok powiedzie sie w polowie zawodnik blokujacy otrzyma polowe obrazen
+			Warrior2->Body[target].receiveDamage(ceil(Warrior1->Body[attackSource].getDamage() / 2));
+		else //jesli blok sie nie powiedzie zawodnik otrzymuje pelne obrazenia
+			Warrior2->Body[target].receiveDamage(Warrior1->Body[attackSource].getDamage());
+	}
+
+	static void Match(Fighter* Warrior1, Fighter* Warrior2)
+	{
+		while (true)
 		{
+			Warrior1->Bleeding(); // na poczatku tury zawodnik otrzymuje obrazenia od krwawienia
 
-			Warrior1.Bleeding(); // na poczatku tury zawodnik otrzymuje obrazenia od krwawienia
+			Warrior1->checkStatusFighter(); //sprawdzamy czy przezyl
+			if (!Warrior1->getStatusFighter())
+				break;
 
-			Warrior1.checkStatusFighter(); //sprawdzamy czy przezyl
-
-			if (Warrior1.getStatusFighter() == false) break;
-			
 			Attack(Warrior1, Warrior2);  //atak
 
-			Warrior1.checkStatusFighter(); //sprawdzenie stanu w1
-			
-			if (Warrior1.getStatusFighter() == false) break;
+			Warrior1->checkStatusFighter(); //sprawdzenie stanu w1
+			if (!Warrior1->getStatusFighter())
+				break;
 
-			Warrior2.checkStatusFighter(); //sprawdzenie stanu w2
-			
-			if (Warrior2.getStatusFighter() == false) break;
+			Warrior2->checkStatusFighter(); //sprawdzenie stanu w2
+			if (!Warrior2->getStatusFighter())
+				break;
 
-		    Warrior2.Bleeding(); // krwawienie
+			Warrior2->Bleeding(); // krwawienie
 
-			Warrior2.checkStatusFighter(); //status drugiego zawodnika po krwawieniu
+			Warrior2->checkStatusFighter(); //status drugiego zawodnika po krwawieniu
+			if (!Warrior2->getStatusFighter())
+				break;
 
-			if (Warrior2.getStatusFighter() == false) break;
-            
 			Attack(Warrior2, Warrior1); //atak drugiego zawodnika
-			
-			Warrior1.checkStatusFighter(); //sprawdzenie statusów po ataku
 
-			if (Warrior1.getStatusFighter() == false) break;
-			
-			Warrior2.checkStatusFighter();
+			Warrior1->checkStatusFighter(); //sprawdzenie statusów po ataku
+			if (!Warrior1->getStatusFighter())
+				break;
 
-			if (Warrior2.getStatusFighter() == false) break;
+			Warrior2->checkStatusFighter();
+			if (!Warrior2->getStatusFighter())
+				break;
 		}
 	}
 };
 
 class Tournament : public Battle {
-	
-	Fighter Duelists[64][64];
-
 public:
-
-	Tournament() {
-		for (int i = 0; i < 64; i++)
+	static Fighter StartFight64(Fighter Duelists[])
+	{
+		for (int k = 0; k < 6; k++)
 		{
-			for (int j = 0; j < 64; j++)
+			int i = 0;
+			int j = N - 1;
+
+			cout << endl << endl << k << endl << endl;
+
+			while (true)
 			{
-				
+				while (true)
+				{
+					if (Duelists[i].getStatusFighter())
+						break;
+					else
+						i++;
+					if (i >= j)
+						break;
+				}
+
+				while (true)
+				{
+					if (Duelists[j].getStatusFighter())
+						break;
+					else
+						j--;
+					if (j <= i)
+						break;
+				}
+
+				if (i >= j)
+					break;
+
+				Match(&(Duelists[i]), &(Duelists[j]));
+
+				cout << "i, j: " << i << " - " << Duelists[i].getStatusFighter() << ",   " << j << " - " << Duelists[j].getStatusFighter() << endl;
+				i++;
+				j--;
 			}
-         
-	    }
+		}
+		for (int t = 0; t < N; t++)
+		{
+			if (Duelists[t].getStatusFighter())
+			{
+				cout << "\n\nZWYCIEZYL NUMER " << t << endl;
+				return Duelists[t];
+			}
+		}
 	}
-
 };
-
-
 
 int main()
 {
 	srand(time(NULL));
-    return 0;
-}
 
+	Fighter Winners[N];
+
+	for (int i = 0; i < N; i++)
+	{
+		Fighter Duelists[N];
+		Winners[i] = Tournament::StartFight64(Duelists);
+	}
+
+	cout << "LET THE SHIT BEGIN!" << endl;
+
+	Tournament::StartFight64(Winners);
+
+	return 0;
+}
